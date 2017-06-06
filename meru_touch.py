89836ebdcd171a99eb55dcc31c3cb2c288e4ddf8
Meru Prabhat <meruprabhat@gmail.com>
2017-06-06 23:00:40 +0530

    Touch with comments

--- meru_touch.py
+++ meru_touch.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 
 import sys,os
+#Getting all arguments as file name except first argument
 a=sys.argv[1:]
-print a
-new=
-
+#Looping through all the file names
 for i in a:
+	#Check Whether the file with same file already exists, if yes through an exception
 	try:
 		a=open(i,'r')
+		#If file is there no need to create new or update the data
 		if (a is True):
-			os.utime(i,(new,new))
-			pass
+		pass
+	#If the file with same filename do ot exists create the file
 	except:
 		f=open(i,"w+")
 		
